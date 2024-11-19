5de0ff155174e1b4ab587677f69d9eea7a5c3554
Likssss <148707565+Likssss@users.noreply.github.com>
2024-11-19 15:11:12 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,31 +38,31 @@ const projects = [
   {
     title: "FitFit",
     description: "A fitness IOS Application with gamelike new features",
-    image: "../../public/assets/503.mp4",
+    image: "/assets/503.mp4",
     href: "https://github.com/Likssss/fitness-app",
   },
   {
     title: "Invoice App",
     description: "A simple Invoice webapp",
-    image: "../../public/assets/503.mp4",
+    image: "/assets/503.mp4",
     href: "https://github.com/Likssss/invoice-app-lix",
   },
   {
     title: "E-Learning Website",
     description: "An E-Learning website with admin and user dashboards",
-    image: "../../public/assets/503.mp4",
+    image: "/assets/503.mp4",
     href: "https://github.com/Likssss/E-Learning-Website",
   },
   {
     title: "NomNom",
     description: "Recipe Finding IOS Application with Image Recognition",
-    image: "../../public/assets/503.mp4",
+    image: "/assets/503.mp4",
     href: "https://github.com/Likssss/nomnom",
   },
   {
     title: "This website",
     description: "My personal website",
-    image: "../../public/assets/503.mp4",
+    image: "/assets/503.mp4",
     href: "https://github.com/Likssss/portfolio",
   },
 ];
